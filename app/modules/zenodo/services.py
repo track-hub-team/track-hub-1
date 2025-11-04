0dfcc1ca42c40028beef60d8b33819db6ab7e265
Adolfo Borrego <adolfo.bglez@gmail.com>
2025-11-04 12:14:51 +0100

    feat: first phase gpx completed
    Refs: SCRUM-61

--- app/modules/zenodo/services.py
+++ app/modules/zenodo/services.py
@@ -209,10 +209,11 @@ class ZenodoService(BaseService):
         """
         Sube un fichero a una deposición existente.
         """
-        uvl_filename = feature_model.fm_meta_data.uvl_filename
-        data = {"name": uvl_filename}
+        filename = feature_model.fm_meta_data.filename
+        data = {"name": filename}
         user_id = current_user.id if user is None else user.id
-        file_path = os.path.join(uploads_folder_name(), f"user_{str(user_id)}", f"dataset_{dataset.id}/", uvl_filename)
+        file_path = os.path.join(uploads_folder_name(), f"user_{str(user_id)}", f"dataset_{dataset.id}/", filename)
+        files = {"file": open(file_path, "rb")}
 
         publish_url = f"{self.ZENODO_API_URL}/{deposition_id}/files"
         try:
