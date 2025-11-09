309d8c948e93f3c8561bcfd24b58588dce5c8854
Juan-Varo <juavarver@alum.us.es>
2025-11-09 18:29:35 +0100

    refactor: refactor communities list page
    Refs: SCRUM-40

--- app/modules/community/repositories.py
+++ app/modules/community/repositories.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from app.modules.community.models import Community, CommunityCurator, CommunityDataset, CommunityRequest
+from app.modules.community.models import Community, CommunityCurator, CommunityDataset
 from core.repositories.BaseRepository import BaseRepository
 
 
@@ -77,39 +77,3 @@ class CommunityDatasetRepository(BaseRepository):
     def get_dataset_communities(self, dataset_id: int) -> List[CommunityDataset]:
         """Obtener todas las comunidades donde está un dataset"""
         return self.model.query.filter_by(dataset_id=dataset_id).all()
-
-
-class CommunityRequestRepository(BaseRepository):
-    def __init__(self):
-        super().__init__(CommunityRequest)
-
-    def get_pending_requests(self, community_id: int) -> List[CommunityRequest]:
-        """Obtener solicitudes pendientes de una comunidad"""
-        return (
-            self.model.query.filter_by(community_id=community_id, status=CommunityRequest.STATUS_PENDING)
-            .order_by(self.model.requested_at.desc())
-            .all()
-        )
-
-    def get_by_community_and_dataset(self, community_id: int, dataset_id: int) -> Optional[CommunityRequest]:
-        """Obtener solicitud específica por comunidad y dataset"""
-        return self.model.query.filter_by(community_id=community_id, dataset_id=dataset_id).first()
-
-    def has_pending_request(self, community_id: int, dataset_id: int) -> bool:
-        """Verificar si existe una solicitud pendiente para un dataset en una comunidad"""
-        return (
-            self.model.query.filter_by(
-                community_id=community_id, dataset_id=dataset_id, status=CommunityRequest.STATUS_PENDING
-            ).first()
-            is not None
-        )
-
-    def get_pending_request_by_dataset(self, community_id: int, dataset_id: int) -> Optional[CommunityRequest]:
-        """Obtener solicitud pendiente específica por comunidad y dataset"""
-        return self.model.query.filter_by(
-            community_id=community_id, dataset_id=dataset_id, status=CommunityRequest.STATUS_PENDING
-        ).first()
-
-    def get_user_requests(self, user_id: int) -> List[CommunityRequest]:
-        """Obtener todas las solicitudes de un usuario"""
-        return self.model.query.filter_by(requester_id=user_id).order_by(self.model.requested_at.desc()).all()
