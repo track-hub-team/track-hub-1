0a6c9cc69c6d56fd3cb247c32b2a23a064b6dd85
PDJ6975 <antrodcal@alum.us.es>
2025-11-09 16:06:00 +0100

    refactor: remove api file
    Refs: SCRUM-41

--- app/modules/community/__init__.py
+++ app/modules/community/__init__.py
@@ -2,5 +2,4 @@ from core.blueprints.base_blueprint import BaseBlueprint
 
 community_bp = BaseBlueprint("community", __name__, template_folder="templates")
 
-# Import routes and API to register them with the blueprint
-from app.modules.community import api, models, routes  # noqa: F401, E402
+from app.modules.community import models, routes  # noqa: F401, E402
