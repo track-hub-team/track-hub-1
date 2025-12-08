e6c93ef9bf9808bd1728945b7cebed9ce6f67014
Pablo Olivencia Moreno <pabolimor@alum.us.es>
2025-12-08 10:48:03 +0000

    fix: use number of feature models instead of max id
    Refs: SCRUM-56

--- app/modules/featuremodel/repositories.py
+++ app/modules/featuremodel/repositories.py
@@ -1,5 +1,3 @@
-from sqlalchemy import func
-
 from app.modules.featuremodel.models import FeatureModel, FMMetaData
 from core.repositories.BaseRepository import BaseRepository
 
@@ -9,8 +7,7 @@ class FeatureModelRepository(BaseRepository):
         super().__init__(FeatureModel)
 
     def count_feature_models(self) -> int:
-        max_id = self.model.query.with_entities(func.max(self.model.id)).scalar()
-        return max_id if max_id is not None else 0
+        return self.model.query.count()
 
 
 class FMMetaDataRepository(BaseRepository):
